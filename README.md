7b6bdf8c364780a9db4d5cf424abdcfd29a3ca96
xiami <xiami417399357@gmail.com>
2022-09-10 21:17:11 +0800

    construct project structure

---
+++ README.md
@@ -0,0 +1,4 @@
+# GenshinDataViewer
+
+# Tips:
+* 需要自己下载[GenshinData](https://github.com/Dimbreath/GenshinData)项目放在resources目录下
---
+++ src/main/java/space/xiami/project/genshindataviewer/Application.java
@@ -0,0 +1,17 @@
+package space.xiami.project.genshindataviewer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@SpringBootApplication
+@Configuration
+@ComponentScan(value = "space.xiami.project.genshindataviewer")
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
